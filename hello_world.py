fe4fbe4fd0b4cc679c987070241d75cf03393254
DiegoMactire <Diego.Mactire@gmail.com>
2021-07-07 22:13:55 -0300

    my second commit

---
+++ hello_world.py
@@ -0,0 +1,23 @@
+#exibe uma saudação simples
+print('\nHello world!\n')
+
+print("Hello world!\n")
+
+hello_world_0 = 'Hello world!\n'
+print(hello_world_0)
+
+hello_world_1 = "Hello world!\n"
+print(hello_world_1)
+
+hello_0 = 'Hello '
+world_0 = 'world!\n'
+print(hello_0 + world_0)
+
+hello_1 = "Hello "
+world_1 = "world!\n"
+print(hello_1 + world_1)
+
+hello_2 = 'Hello '
+world_2 = 'world!\n'
+message = hello_2 + world_2
+print(message)
